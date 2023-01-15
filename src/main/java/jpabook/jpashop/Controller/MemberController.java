f6f2e717a927a615af0cb5d3b2c44618c339b039
junwon1997 <junwon20170339@gmail.com>
2023-01-16 01:33:22 +0900

    상품 등록,목록조회,수정 기능 개발

--- src/main/java/jpabook/jpashop/Controller/MemberController.java
+++ src/main/java/jpabook/jpashop/Controller/MemberController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,12 +20,20 @@ public class MemberController {
 
     private final MemberService memberService;
 
+
+    /**
+     * 회원 가입 작성 폼 불러오기
+     */
     @GetMapping("members/new")
     public String createForm(Model model){
         model.addAttribute("memberForm", new MemberForm());
         return "members/createMemberForm";
     }
 
+
+    /**
+     * 회원 가입
+     */
     @PostMapping ("members/new")
     public String create(@Valid MemberForm form, BindingResult result){
 
@@ -40,5 +49,16 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    /**
+     * 회원 목록 조회
+     */
+    @GetMapping("/members")
+
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
     }
 }
