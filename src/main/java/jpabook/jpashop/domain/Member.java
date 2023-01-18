30470d89324b4465e398cb9e55971127568832aa
junwon1997 <junwon20170339@gmail.com>
2023-01-18 17:34:45 +0900

    회원 관련 api 개발

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,9 +1,11 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,10 +18,13 @@ public class Member {
     @Id @GeneratedValue
     @Column(name = "member_id")
     private Long id;
+
+    @NotEmpty
     private String name;
-    private Time datetime;
+
     @Embedded
     private Address address;
+
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
